d6c72794ae8f5e1680982e53fa17b396965a8908
majorazero <daniel.cw.hsu@gmail.com>
2018-06-20 17:59:42 -0700

    added an about page template

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
+      <Link to="/about" activeClassName="active">About</Link>
     </nav>
   );
-}
+};
 
 export default Header;
